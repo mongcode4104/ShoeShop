9b57acc7f7477b2b409a35f52803a548df09876e
mongc <mongc@MONGPC>
2023-07-13 13:01:02 +0700

    add filter status and format data list

---
+++ ShoeShop.Web/app/shared/filters/statusFilter.js
@@ -0,0 +1,10 @@
+﻿(function (app) {
+    app.filter('statusFilter', function () {
+        return function (input) {
+            if (input == true)
+                return 'Kich hoat';
+            else
+                return 'Khoa';
+        }
+    });
+})(angular.module('shoeshop.common'))
